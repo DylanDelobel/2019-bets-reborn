81b187199eaed82d49f588cfa0342033355d5619
DylanDelobel <dylan.delobel66@gmail.com>
2019-05-10 19:13:35 +0200

    Add port env variable

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@ import os
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from app.flask_app import create_app, db
@@ -12,7 +13,7 @@ from app.model import user
 
 from app import blueprint
 
-app = create_app(os.getenv('PROJECT_ENV') or 'dev')
+app = create_app(os.getenv('PROJECT_ENV') or 'prod')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
@@ -23,11 +24,15 @@ migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
 
 @manager.command
 def run():
-    app.run(port=8000)
+    app.run(port)
 
 
 if __name__ == '__main__':
-    manager.run()
+    manager.run(port)
