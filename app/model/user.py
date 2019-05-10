58e5f39902037ad93e43692afcb76909fe69587e
Dylan Delobel <dylan.delobel66@gmail.com>
2019-05-10 16:02:27 +0200

    Add user auth

--- app/model/user.py
+++ app/model/user.py
@@ -1,4 +1,7 @@
 from ..flask_app import db, flask_bcrypt
+import datetime
+import jwt
+from ..config import key
 
 
 class User(db.Model):
@@ -26,3 +29,38 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+    @staticmethod
+    def encode_auth_token(user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
